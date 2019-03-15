320e0bc01e57d6a0c968ddb49641da62b5c1d8fb
manertou <manertou@163.com>
2019-03-15 19:07:52 +0800

    log.js modify

--- log.js
+++ log.js
@@ -1,10 +1,16 @@
 const fs = require('fs');
+const moment = require('moment');
 const globalConf = require('./config');
 
 const fileName = globalConf.log_path + globalConf.log_name;
 
 function log(data) {
-    const time = new Date();
-    fs.appendFile(fileName, time + '\n' + data + '\n\n', {flag: 'a'}, function () {})
+    fs.appendFile(fileName, nowDate() + data + '\n', {flag: 'a'}, function () {})
 }
+
+function nowDate() {
+    const formatDate = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
+    return formatDate + ' ' + Date.now() + ' '
+}
+
 module.exports = log;
